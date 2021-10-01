2c2c22aeda654064750c18ef461f686829a818af
Minh Hai <ngminhhai272@gmail.com>
2021-10-01 22:38:30 +0700

    test branch 3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const authRouter = require('./routes/auth')
 const postRouter = require('./routes/post')
 
@@ -23,6 +23,8 @@ connectDB();
 
 const app = express()
 app.use(express.json())
+app.use(cors())
+
 app.use('/api/auth', authRouter)
 app.use('/api/post', postRouter)
 
